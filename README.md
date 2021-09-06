1229b7978cd19c6bf4838c5e903d0f237f8c4079
Taufik Hidayat <taufik@fiqhidayat.com>
2021-09-06 22:19:14 +0700

    Fix unable to check update

--- README.md
+++ README.md
@@ -19,6 +19,10 @@ For backwards compatibility, if this section is missing, the full length of the 
 Markdown parsed.
 
 == Changelog ==
+= 1.0.2 =
+
+* Fix unable to update checker
+
 = 1.0.1 =
 
 * Fixed bug display debuging message
--- salesloo-tripay.php
+++ salesloo-tripay.php
@@ -15,7 +15,7 @@
  * Plugin Name:       Salesloo Tripay
  * Plugin URI:        https://www.salesloo.com
  * Description:       Indonesian Payment gateway for Salesloo
- * Version:           1.0.1
+ * Version:           1.0.2
  * Author:            Salesloo
  * Author URI:        https://www.salesloo.com
  * License:           GPL-2.0+
@@ -34,10 +34,12 @@ if (!defined('WPINC')) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define('SALESLOO_TRIPAY_VERSION', '1.0.1');
+define('SALESLOO_TRIPAY_VERSION', '1.0.2');
 define('SALESLOO_TRIPAY_URL', plugin_dir_url(__FILE__));
 define('SALESLOO_TRIPAY_PATH', plugin_dir_path(__FILE__));
 define('SALESLOO_TRIPAY_ROOT', __FILE__);
+
+require 'update-checker/plugin-update-checker.php';
 
 /**
  * The code that runs during plugin activation.
